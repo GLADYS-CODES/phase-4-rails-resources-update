85c48848eeb230db387fddde4f52ad4ff1f32abd
GLADYS <mungaiglado106@gmail.com>
2022-09-26 16:02:30 +0300

    inital commit

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,5 +1,35 @@
 class BirdsController < ApplicationController
 
+  # find the bird that matches the ID from the route params
+    # update the bird using the params from the body of the request
+  #PATCH /birds/:id
+
+def update
+  bird = Bird.find_by(id: params[:id])
+  if bird
+    bird.update(bird_params)
+    render json: bird
+    
+  else
+    
+    render json: { error: "Bird not found"}, status: :not_found
+    end
+  end
+  
+
+  #do the work of calculating the number of likes and incrementing it. custom route
+  def increment_likes
+    bird = Bird.find_by(id: params[:id])
+    if bird
+      bird.update(likes: bird.likes + 1)
+      render json: bird
+    else
+      render json: { error: "Bird not found" }, status: :not_found
+    end
+  end
+
+
+
   # GET /birds
   def index
     birds = Bird.all
